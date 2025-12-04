92e13f3294716122a9f204c97a4ec66d37ca34f8
Shreyasdubeyy <shreyasdubey03@gmail.com>
2025-12-04 23:39:26 +0530

    Feature for viewing all accepted connections

--- Project/Backend/src/routes/UserRouter.js
+++ Project/Backend/src/routes/UserRouter.js
@@ -3,6 +3,8 @@ const jwtAuthCheck = require("../Middleware/JwtAuthetication")
 const ConnectionRequest=require("../models/ConnectionModel")
 const userRouter=express.Router()
 
+const USER_DATA_ALLOWED="firstName lastName age about skills"
+
 userRouter.get("/received/requests",jwtAuthCheck,async(req,res)=>{
 try {
 const loggedInUser=req.user;
@@ -21,6 +23,32 @@ res.send(requests)
     })
 }
 
+})
+
+userRouter.get("/connections",jwtAuthCheck,async(req,res)=>{
+    try {
+        const loggedInUser=req.user;
+        const connections=await ConnectionRequest.find({
+            $or:[{SenderId:loggedInUser._id},{ReceiverId:loggedInUser._id}],
+            status:"accepted"
+        }).populate("SenderId",USER_DATA_ALLOWED).populate("ReceiverId",USER_DATA_ALLOWED)
+
+
+        const filteredConnections=connections.map((row)=>{
+            if(row.SenderId._id.toString()===loggedInUser._id.toString()){
+                return row.ReceiverId
+            }
+            return row.SenderId
+        })
+
+        res.send("Friends:"+JSON.stringify(filteredConnections,null,2))
+
+
+    } catch (error) {
+        res.status(400).json({
+            message:error.message
+        })
+    }
 })
 
 
