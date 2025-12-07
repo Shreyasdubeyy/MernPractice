9660c31ea98ca45d906fbfc7184ffc26b8db5e1a
Shreyasdubeyy <shreyasdubey03@gmail.com>
2025-12-07 23:45:12 +0530

    Installed react router dom and Created an Outlet

--- Project/Frontend/src/App.jsx
+++ Project/Frontend/src/App.jsx
@@ -1,7 +1,20 @@
-import Navbar from "./components/Navbar";
+import {BrowserRouter,Routes,Route} from "react-router-dom"
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 export default function App() {
   return (
-  <Navbar/>
+  <BrowserRouter browsename="/">
+    <Routes>
+      <Route path="/"  element={<Body/>}>
+      
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+
+      
+      </Route>
+    </Routes>
+  </BrowserRouter>
   );
 }
