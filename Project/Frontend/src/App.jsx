63af3257115a5daaaa9aedcb9a6c9bd98eaa9608
Shreyasdubeyy <shreyasdubey03@gmail.com>
2025-12-09 00:44:34 +0530

    Used Redux toolkit,useDispatch hook,useNavigate hook

--- Project/Frontend/src/App.jsx
+++ Project/Frontend/src/App.jsx
@@ -2,13 +2,18 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 import Body from "./components/Body";
 import Login from "./components/Login";
 import Profile from "./components/Profile";
+import {Provider} from "react-redux"
+import appStore from "./utils/AppStore";
+import Feed from "./components/Feed";
 
 export default function App() {
   return (
-  <BrowserRouter browsename="/">
+  <Provider store={appStore}> 
+
+<BrowserRouter basename="/">
     <Routes>
       <Route path="/"  element={<Body/>}>
-      
+      <Route path="/feed" element={<Feed/>}/>
       <Route path="/login" element={<Login/>}/>
       <Route path="/profile" element={<Profile/>}/>
 
@@ -16,5 +21,7 @@ export default function App() {
       </Route>
     </Routes>
   </BrowserRouter>
+
+  </Provider>
   );
 }
--- Project/Frontend/src/components/Login.jsx
+++ Project/Frontend/src/components/Login.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
 import {useState} from "react"
 import axios from "axios"
+import { useDispatch } from 'react-redux';
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from 'react-router-dom';
+import { BASE_URL } from '../constants';
+
 
 
 const Login = () => {
 
-const [email,setEmail]=useState("shreyasdubey@gmail.com");
+const [email,setEmail]=useState("shreyas@gmail.com");
 const [password,setPassword]=useState("12345");
+
+const dispatch=useDispatch()
+
+const navigate=useNavigate()
 
 const handleLogin=async()=>{
   try {
-    const res=await axios.post("http://localhost:3000/login",{
+    const res=await axios.post(
+      BASE_URL+"/login",
+    {
       email,password
     },{
       withCredentials:true
     })
     console.log(res)
+    dispatch(addUser(res.data.user))
+    return navigate("/feed")
+
+
   } catch (error) {
     console.log(error.message)
   }
